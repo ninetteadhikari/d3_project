f35283e6cff921bfcc53bf60c67982d2069f247c
Ninette Adhikari <ninette.adhikari@gmail.com>
2020-11-26 16:34:53 +0600

    feat: Add styles to create bars

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ const DUMMY_DATA = [
     {id: 'd4', value: 6, region:'Germany'},
 ]
 
-d3.select('div')
-    .selectAll('p')
-    .data(DUMMY_DATA) // data is bound to the paragraph
-    .enter() // this gives all the missing elements
-    .append('p') // add missing elements ('p') to the parent ('div'). An element is added for each data point
-    // .text('Hello')
-    .text(data => data.region)
+const container = d3.select('div')
+    // .selectAll('p')
+    // .data(DUMMY_DATA) // data is bound to the paragraph
+    // .enter() // this gives all the missing elements
+    // .append('p') // add missing elements ('p') to the parent ('div'). An element is added for each data point
+    // // .text('Hello')
+    // .text(data => data.region)
+    .classed('container', true)
+    .style('border', '1px solid red')
+
+const bars = container
+    .selectAll('.bar')
+    .data(DUMMY_DATA)
+    .enter()
+    .append('div')
+    .classed('bar', true)
+    .style('width','50px')
+    .style('height', '150px')
