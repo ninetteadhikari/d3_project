387c9042d79931b6b606eb4d2c991df34f07acef
Ninette Adhikari <ninette.adhikari@gmail.com>
2020-11-26 16:25:34 +0600

    feat: Add dummy data

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
+const DUMMY_DATA = [
+    {id: 'd1', value: 10, region:'USA'},
+    {id: 'd2', value: 11, region:'India'},
+    {id: 'd3', value: 12, region:'China'},
+    {id: 'd4', value: 6, region:'Germany'},
+]
+
 d3.select('div')
     .selectAll('p')
-    .data([1,2,3]) // data is bound to the paragraph
+    .data(DUMMY_DATA) // data is bound to the paragraph
     .enter() // this gives all the missing elements
     .append('p') // add missing elements ('p') to the parent ('div'). An element is added for each data point
     // .text('Hello')
-    .text(data => data)
+    .text(data => data.region)
