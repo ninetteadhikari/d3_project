ae5c72bfe2494f934937eba43f49adcfe24edbe5
Ninette Adhikari <ninette.adhikari@gmail.com>
2020-11-26 16:21:49 +0600

    feat: Render text from data array

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 d3.select('div')
     .selectAll('p')
-    .data([1,2,3])
+    .data([1,2,3]) // data is bound to the paragraph
+    .enter() // this gives all the missing elements
+    .append('p') // add missing elements ('p') to the parent ('div'). An element is added for each data point
+    // .text('Hello')
+    .text(data => data)
