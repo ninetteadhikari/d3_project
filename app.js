47e1f5ce4e0df0442b405e5f6c175bc8cbe2a3fc
Ninette Adhikari <ninette.adhikari@gmail.com>
2020-11-30 17:19:43 +0600

    feat: Add scale and axis to chart

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const DUMMY_DATA = [
-    {id: 'd1', value: 10, region:'USA'},
-    {id: 'd2', value: 11, region:'India'},
-    {id: 'd3', value: 12, region:'China'},
-    {id: 'd4', value: 6, region:'Germany'},
-]
+  { id: 'd1', value: 10, region: 'USA' },
+  { id: 'd2', value: 11, region: 'India' },
+  { id: 'd3', value: 12, region: 'China' },
+  { id: 'd4', value: 6, region: 'Germany' },
+];
 
-const container = d3.select('svg')
-    .classed('container', true)
+const xScale = d3
+  .scaleBand()
+  .domain(DUMMY_DATA.map((datapoint) => datapoint.region))
+  .rangeRound([0, 250])
+  .padding(0.1); // scaleBand gives uniform distribution
+const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
 
+const container = d3.select('svg').classed('container', true);
 
 const bars = container
-    .selectAll('.bar')
-    .data(DUMMY_DATA)
-    .enter()
-    .append('rect')
-    .classed('bar', true)
-    .attr('width', 50)
-    .attr('height', data => data.value * 15)
+  .selectAll('.bar')
+  .data(DUMMY_DATA)
+  .enter()
+  .append('rect')
+  .classed('bar', true)
+  .attr('width', xScale.bandwidth())
+  .attr('height', (data) => 200 - yScale(data.value))
+  .attr('x', (data) => xScale(data.region))
+  .attr('y', (data) => yScale(data.value));
